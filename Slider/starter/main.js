f69dbfcc1f06571148a8d5b395585d8cf7310d06
Laurian Mathiot <132049556+LaurianMathiot@users.noreply.github.com>
2023-06-20 09:41:37 +0200

    end of slider

--- Slider/starter/main.js
+++ Slider/starter/main.js
@@ -2,16 +2,81 @@ const slidesData = [
     { title: "VILLAGE", description: "Lorem ipsum dolor sit amet" },
     { title: "VIGNE", description: "Consectetur adipiscing elit" },
     { title: "CHAMPS", description: "Sed do eiusmod tempor incididunt" },
-]
+];
 
-const leftBTN = document.querySelector(".slider-btn-left")
-const rightBTN = document.querySelector(".slider-btn-right")
-let slideIndex = 0
+const btns = document.querySelectorAll(".slider-btn");
+const imgs = document.querySelectorAll(".slider-img");
+const dots = document.querySelectorAll(".dot");
 
+let slideIndex;
 
-leftBTN.addEventListener('click', onClickBtn)
-rightBTN.addEventListener('click', onClickBtn)
+init();
 
-let onClickBtn = (e) => {
+btns.forEach((el, index) => {
+    el.maNouvellePropriete = 'nouvellePropriete_' + index + '!'
+    el.leftOrRight = index;
+    el.addEventListener("click", onClickBtn);
+    // e.addEventListener("mousedown", onDownBtn);
+    // e.addEventListener("mouseup", onUpBtn);
+});
 
+dots.forEach((dot, ind) => {
+    dot.indexPosition = ind
+    dot.addEventListener('click', onClickDot)
+})
+
+function onClickDot(e) {
+    slideIndex = e.currentTarget.indexPosition
+    updateCurrentImage(slideIndex)
+}
+
+function onDownBtn(e) {
+    console.log('down')
+}
+
+function onUpBtn(e) {
+    console.log('up')
+}
+
+function onClickBtn(e) {
+    if (e.currentTarget.leftOrRight === 0) {
+        slideIndex--;
+    } else {
+        slideIndex++;
+    }
+
+    updateCurrentImage(slideIndex)
+}
+
+function updateCurrentImage(slideI) {
+    btns[0].style.opacity = btns[1].style.opacity = 1
+    btns[0].style.pointerEvents = btns[1].style.pointerEvents = 'auto'
+
+    if (slideI <= 0) {
+        slideI = 0;
+        btns[0].style.opacity = 0.3
+        btns[0].style.pointerEvents = 'none'
+    } else if (slideI >= imgs.length - 1) {
+        slideI = imgs.length - 1;
+        btns[1].style.opacity = 0.3
+        btns[1].style.pointerEvents = 'none'
+    }
+
+    displayImages(slideI)
+}
+
+function displayImages(slideI) {
+    imgs.forEach((img, indexImg) => {
+        img.style.transition = 'transform 3s';
+        img.style.transform = `translateX(${(indexImg - slideI) * 100}%)`
+    })
+}
+
+function init() {
+    slideIndex = 0;
+    btns[0].style.opacity = 0.5
+    btns[0].style.pointerEvents = 'none'
+    imgs.forEach((img, indexImg) => {
+        img.style.transform = `translateX(${(indexImg - slideIndex) * 100}%)`
+    })
 }
